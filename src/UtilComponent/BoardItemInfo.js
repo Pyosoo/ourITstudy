cf493c0412300c7d124d62d1e3b40efb38091b8e
전표수 <snstkfka02@hanmail.net>
2020-11-13 02:08:50 +0900

    reply add & delete update

--- src/UtilComponent/BoardItemInfo.js
+++ src/UtilComponent/BoardItemInfo.js
@@ -6,108 +6,148 @@ import { auth } from '../firebase_config';
 import trash from '../Images/trash.png';
 
 let temp = [];
-function BoardItemInfo(props){
+let flag = false;
+function BoardItemInfo(props) {
     const location = useLocation();
     const ItemInfo = location.state.ItemInfo;
-            /* ItemInfo 정보
-            id, email, write, title, content, position, region, mine, likePeople, number, day
-            */
+    /* ItemInfo 정보
+    id, email, write, title, content, position, region, mine, likePeople, number, day
+    */
     console.log(ItemInfo.id + '값을 받아옵니다');
 
     const [review, setreview] = useState(''); //댓글창에 쓰는 input
-    const [ItemReview, setItemReview] = useState([]); // 현재 글에 대한 댓글이 모여있는 배열
     const [showReview, setshowReview] = useState([]); // 화면에 보여줄 객체 (댓글들을 map을 통해 p만듬)
-    
-    
+
+
     const handleReviewChange = e => {
         setreview(e.target.value);
     }
     const AddReview = () => {
         // 기존의 ItemReview에 새로 입력한 댓글을 추가하기
-        let newItemReview = [...ItemReview, {who:auth.currentUser.email, what:review}];
         // WillUnmount에서 저장하기 위해 별도로 저장한번 더
-        temp = [...temp, {who:auth.currentUser.email, what:review}];
+        let check = window.confirm("댓글을 등록하시겠습니까?");
+        if(check){
+            temp = [...temp, { who: auth.currentUser.email, what: review }];
 
-        // HTML에 보여질 요소로 만들어 주기
-        let ReviewItem = newItemReview.map((data,index) =>
-            <div className="Review_line" key={index}>
-                <p className="Review_what">{data.what}</p>
-                <p className="Review_who">{data.who}</p>
-            </div>
-        )
-        setshowReview(ReviewItem);
-        setItemReview(newItemReview)
-        setreview('');
-        let newItemInfo = {...ItemInfo, reply : temp}
-        database.ref().child('boards').child(ItemInfo.id).set(
-            newItemInfo
-         );
-         temp = [];
+            // HTML에 보여질 요소로 만들어 주기
+            let ReviewItem = temp.map((data, index) =>
+                <div className="Review_line" key={index}>
+                    <p className="Review_what">{data.what}</p>
+                    <p className="Review_who">{data.who}</p>
+                </div>
+            )
+            setshowReview(ReviewItem);
+            setreview('');
+            let newItemInfo = { ...ItemInfo, reply: temp }
+            database.ref().child('boards').child(ItemInfo.id).set(
+                newItemInfo
+            );
+            flag = true;//여기서 데이터를 추가해버리면. 나갈때 다시 빈 배열로 초기화를 시켜버린다....
+        }
+    }
+
+    const DeleteReview = (_id) => {
+        let check = window.confirm("정말 삭제하시겠습니까?");
+        if(check === true){
+            let Data;
+            // 우선 해당글에 대한 댓글을 모두 받아온 후
+            reference.on('value', snapshot => {
+                for (let key in snapshot.val().boards) {
+                    if (key === ItemInfo.id) { // ItemInfo.id => AIISNAD@AI2 같은 해당 글에 대한 ID값
+                        Data = snapshot.val().boards[key];
+                        break;
+                    }
+                }
+            })
+            console.log(Data.reply);
+    
+            // 클릭된 id 값을 가진 요소를 지우고
+            Data.reply = Data.reply.filter((data, index) => index !== parseInt(_id));
+            if(Data.reply.length === 0){
+                setshowReview([]);
+                temp = [];
+            }
+            let newItemInfo = { ...ItemInfo, reply: Data.reply }
+            database.ref().child('boards').child(ItemInfo.id).set(
+                newItemInfo
+            );
+        }
+       
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
     // firebase 관련
-    
+
     let database = firebase.database();
     let reference = database.ref(`/`);
 
-    useEffect(()=>{
+    useEffect(() => {
         reference.on('value', snapshot => {
-            //console.log(snapshot.val());
             let Data;
-            for(let key in snapshot.val().boards){
-                if(key === ItemInfo.id){
-                    Data = snapshot.val().boards[key]
-                    //console.log(snapshot.val().boards[key])
+            for (let key in snapshot.val().boards) {
+                if (key === ItemInfo.id) {
+                    Data = snapshot.val().boards[key];
                     break;
                 }
             }
-            if(Data.reply === undefined){
+            console.log(Data.reply);
+            if (Data.reply === undefined) {
                 console.log("아직 리뷰가 없습니다")
-            }else{
+            } else {
                 temp = Data.reply;
                 let makeHTML = Data.reply.map((data, index) => {
-                    console.log(data.who + auth.currentUser.email)
-                    if(data.who === auth.currentUser.email){
+                    if (data.who === auth.currentUser.email) {
                         return (
                             <div className="Review_line" key={index}>
                                 <p className="Review_what">{data.what}</p>
                                 <p className="Review_who">{data.who}</p>
-                                <img alt="" src={trash} className="Review_delete" />
+                                <img id={index} alt="" src={trash} className="Review_delete" onClick={(e) => { DeleteReview(e.currentTarget.id) }} />
                             </div>
                         )
                     }
-                    else{
-                        return(
+                    else {
+                        return (
                             <div className="Review_line" key={index}>
                                 <p className="Review_what">{data.what}</p>
                                 <p className="Review_who">{data.who}</p>
                             </div>
                         )
-                        
+
                     }
                 }
                 )
                 setshowReview(makeHTML);
-                setItemReview(temp);
             }
         })
 
-        return()=>{
-            let newItemInfo = {...ItemInfo, reply : temp}
-           // console.log(newItemInfo);
-          //    console.log(ItemReview); []로나오는 이유는 useEffect의 return에서의 props와 state는 초기 didmount때의 값을 가지고 있기때문이란다..
-            database.ref().child('boards').child(ItemInfo.id).set(
-               newItemInfo
-            );
+        return () => {
+            let newItemInfo = { ...ItemInfo, reply: temp }
+            if (flag === false) {
+                console.log("언마운트시 데이터 다시 저장")
+                database.ref().child('boards').child(ItemInfo.id).set(
+                    newItemInfo
+                );
+            }
             temp = [];
         }
-    },[])
-    
-    
-    
-    
-    return(
+    }, [])
+
+
+
+
+    return (
         <div className="BoardItemInfo_back">
             <p className="BoardItemInfo_title">{ItemInfo.title}</p>
             <div className="BoardItemInfo_header">
